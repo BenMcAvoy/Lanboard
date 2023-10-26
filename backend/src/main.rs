375b3ab832bde8d04108e91c90c98ffcaa4494f9
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-26 03:28:13 +0100

    Create an example time to test protobuf

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,6 +2,24 @@ pub mod api {
     include!(concat!(env!("OUT_DIR"), "/api.v1.rs"));
 }
 
+#[inline]
+fn get_epoch() -> i32 {
+    match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) {
+        Ok(v) => v.as_secs_f32() as i32,
+        Err(e) => {
+            eprintln!("Failed to get unix epoch. Something is very wrong.\n    {e}");
+            std::process::exit(-1);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, backend.")
+    println!("Hello, backend.");
+
+    let mut time = api::Time::default();
+
+    time.name = String::from("Me");
+    time.epoch = get_epoch();
+
+    dbg!(time);
 }
