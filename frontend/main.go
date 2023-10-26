3288cd1d724e878a5c570c897933bdc15dfcfa47
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-26 05:23:42 +0100

    Send a message to the server

--- frontend/main.go
+++ frontend/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	// "google.golang.org/protobuf/proto"
+	"context"
+	"flag"
+	// "fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	// "google.golang.org/grpc/credentials/insecure"
+
+  api "lanboard/protobufs"
+)
+
+var (
+       addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
 
 func main() {
-  fmt.Println("Hello, frontend.");
+  score := &api.Score {
+    Name: "Me",
+    Epoch: int32(time.Now().Unix()),
+  }
+
+  flag.Parse();
+
+  conn, err := grpc.Dial(*addr, grpc.WithInsecure());
+
+  if err != nil {
+    log.Fatalf("Did not connect: %v", err);
+  }
+
+  defer conn.Close();
+
+  client := api.NewLeaderboardClient(conn);
+
+  resp, err := client.Insert(context.Background(), score);
+
+  if err != nil {
+    log.Fatalf("Could not add score: %v", err);
+  }
+
+  log.Printf("Response: %v", resp);
 }
