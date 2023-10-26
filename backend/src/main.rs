41b660e2215d87cc30a47ce30f6f6e2bb8630b81
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-26 05:40:45 +0100

    Add some logging into the server loop

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,11 @@
+const HOST: &str = "0.0.0.0";
+const PORT: &str = "50051";
+
 pub mod api {
     include!(concat!(env!("OUT_DIR"), "/api.v1.rs"));
 }
+
+use tracing::{Level, info};
 
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
@@ -16,7 +21,7 @@ pub struct LeaderboardImpl {}
 #[tonic::async_trait]
 impl Leaderboard for LeaderboardImpl {
     async fn insert(&self, request: Request<Score>) -> Result<Response<InsertionResponse>, Status> {
-        println!("New request: {:#?}", request.into_inner());
+        info!("New request: {:#?}", request.into_inner());
 
         Ok(Response::new(InsertionResponse {
             result: InsertionResult::Okay as i32,
@@ -26,8 +31,17 @@ impl Leaderboard for LeaderboardImpl {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+    tracing_subscriber::fmt()
+        .with_thread_names(true)
+        .with_line_number(true)
+        .with_max_level(Level::INFO)
+        .without_time()
+        .init();
+
+    let addr = format!("{HOST}:{PORT}").parse()?;
     let leaderboard = LeaderboardImpl::default();
+
+    info!("Listening on {addr}");
 
     Server::builder()
         .add_service(LeaderboardServer::new(leaderboard))
